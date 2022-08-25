40a919e00bb62228efb685d981010f910efee29a
BaoNguyen7499 <67630732+BaoNguyen7499@users.noreply.github.com>
2022-08-25 16:34:33 +0700

    master-test

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello React!</p>
+        <h1>Hello React!</h1>
+        <p>Test circleci</p>
       </header>
     </div>
   );
